38f184eaf5d171a64b2f4311b485b942a5a6e2de
mechmedved <unclebear@yandex.ru>
2024-09-25 14:59:18 +0300

    Вынес Scanner scanner в метод main Добавил проверку деления на ноль

--- src/Task5.java
+++ src/Task5.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Task5 {
     public static void main(String[] args) {
-        System.out.println(calc(getInt(),getInt(),getOperation()));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(calc(getInt(scanner),getInt(scanner),getOperation(scanner)));
+        scanner.close();
     }
 
-    public static int getInt(){
-        Scanner scanner = new Scanner(System.in);
+    public static int getInt(Scanner scanner){
         return scanner.nextInt();
     }
 
-    public static char getOperation(){
-        Scanner scanner = new Scanner(System.in);
+    public static char getOperation(Scanner scanner){
         return scanner.next().charAt(0);
     }
 
@@ -25,6 +25,9 @@ public class Task5 {
             case '*':
                 return a * b;
             case '/':
+                if(b == 0){
+                    System.out.println("Division by zero");
+                }
                 return (float) a / b;
             default:
                 System.out.println("Unknown operation");
